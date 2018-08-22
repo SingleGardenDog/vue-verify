acf54d85eba4bc7eb50b2fdd2d98a95f4f46ec61
fangjiuhong <897407836@qq.com>
2018-08-22 09:21:00 +0800

    M:提交新版本验证

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-import verify from "./src/verify";
+"use strict";
 
-if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.use(verify);
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _verify = require("./src/verify.js");
+
+var _verify2 = _interopRequireDefault(_verify);
+
+function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : { default: obj };
 }
 
-export default verify;
+if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.use(_verify2.default);
+}
+
+exports.default = _verify2.default;
